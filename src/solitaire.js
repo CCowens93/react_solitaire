a40cd150714f2156b3bf9421646b6e32a69e7faa
CCowens93 <cassandracowens93@gmail.com>
2020-01-13 12:34:59 -0500

    making outline for board

--- src/solitaire.js
+++ src/solitaire.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 class Solitaire extends Component{
     render(){
         return(
+            <div class="gameboard">
             <div class="container">
                 <div class="deck"></div>
                 
@@ -12,6 +13,17 @@ class Solitaire extends Component{
                     <div id="diamond"></div>
                     <div id="club"></div>
                     <div id="heart"></div>
+                </div>
+            </div>
+
+                <div class="player_stack">
+                    <div id="stack1"></div>
+                    <div id="stack2"></div>
+                    <div id="stack3"></div>
+                    <div id="stack4"></div>
+                    <div id="stack5"></div>
+                    <div id="stack6"></div>
+                    <div id="stack7"></div>
                 </div>
             
             </div>
